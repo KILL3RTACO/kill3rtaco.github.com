f94629f4b15530e5027025c8327d39c392cf746c
KILL3RTACO <kill3rtaco@gmail.com>
2012-12-24 23:44:34 -0800

    sticky scrolls works. moved to own file

--- js/sticky-scroll.js
+++ js/sticky-scroll.js
@@ -1,8 +1,15 @@
-var sticky = document.querySelector('#info-page-sidebar');
-var origOffsetY = sticky.offsetTop;
-
-function onScroll(e) {
-  window.scrollY >= origOffsetY ? sticky.classList.add('fixed') : sticky.classList.remove('fixed');
+function sticky_relocate() {
+	var window_top = $(window).scrollTop();
+	var div_top = $('#sticky-anchor').offset().top;
+	if (window_top > div_top){
+		$('#info-page-sidebar').removeClass('unstick');
+		$('#info-page-sidebar').addClass('stick');
+	}else{
+		$('#info-page-sidebar').removeClass('stick');
+		$('#info-page-sidebar').addClass('unstick');
+	}
 }
-
-document.addEventListener('scroll', onScroll);
+$(function() {
+	$(window).scroll(sticky_relocate);
+	sticky_relocate();
+});
